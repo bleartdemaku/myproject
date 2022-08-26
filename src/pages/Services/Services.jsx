7518816be5056c91090e20efefe5eb2ded3c46fd
bleartdemaku <bd47165@ubt-uni.net>
2022-08-26 22:49:32 +0200

    added dummy api with posts

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import NavbarBootstrap from "../../components/Navbar/NavbarBootstrap";
+import Footer from "../../components/Footer/Footer";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/esm/Container";
 
 function Services() {
+  const [posts, setPosts] = useState("");
+
+  const getApiData = async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts"
+    ).then((response) => response.json());
+
+    setPosts(response);
+  };
+
+  useEffect(() => {
+    getApiData();
+  }, []);
+
   return (
-    <div>
-      <h1>this is services page!!</h1>
-    </div>
-  )
+    <>
+      <NavbarBootstrap />
+      <Container className="mt-3">
+        {posts &&
+          posts.map((post) => (
+            <Card className="text-center mt-3">
+              <Card.Header>{post.id}</Card.Header>
+              <Card.Body>
+                <Card.Title>{post.title}</Card.Title>
+                <Card.Text>{post.body}</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+              <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            </Card>
+          ))}
+      </Container>
+      <Footer />
+    </>
+  );
 }
 
-export default Services
+export default Services;
