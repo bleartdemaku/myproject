1ebf680a6f82f91d73a2517aaea3daac6c7e2c41
bleartdemaku <bd47165@ubt-uni.net>
2022-09-06 19:39:44 +0200

    fixed navbar text not centered

--- src/components/Navbar/NavbarBootstrap.jsx
+++ src/components/Navbar/NavbarBootstrap.jsx
@@ -28,7 +28,7 @@ function NavbarBootstrap() {
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
-              className="me-auto my-2 my-lg-0"
+              className="me-auto my-2 my-lg-0 text-center"
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
